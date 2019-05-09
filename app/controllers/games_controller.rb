2287d3a7a05ec40f8bc08436c45e3bdba3dd7cc2
Kieran Doyle <kieranhbdoyle@gmail.com>
2019-05-09 20:59:44 +0100

    added results message including checking dictionary api for word validity

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,16 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
 
   def included?(guess, grid)
     guess.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
   end
 
 
@@ -16,17 +25,22 @@ class GamesController < ApplicationController
   def score
     @word = params[:word]
     @letters = params[:letters]
-    @congrats = ""
+    @result = ""
 
     @letter_array = @letters.split(" ")
     @word_letters = @word.upcase.split("")
 
-  if included?(@word_letters, @letter_array)
-    @congrats = "Congrats"
+    if included?(@word_letters, @letter_array)
+      @congrats = "Congrats"
+        if english_word?(@word)
+          @result = "Congrats - #{@word} is a valid a word 🤗"
+        else
+          @result = "Sorry - #{@word} is not an English word 🤔"
+        end
+      else
+      @result = "The letters for #{@word} weren't all in the grid ☹️"
+      end
   end
 
     # check if words are in the grid
-  end
-
-
 end
